d554c988d30007c427acbd7cafdc5bc51453fc0b
Louis Betsch <louisbetsch.b@gmail.com>
2024-08-10 18:44:42 +0200

    restructuring

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ echo loading latest imagenet checkpoint
 curl -L https://dl.fbaipublicfiles.com/ijepa/IN1K-vit.h.14-300e.pth.tar > checkpoints/IN1K-vit.h.14-300e.pth.tar
 
 echo loading dataset
-chmod +x datasets/make_dataset.sh
-./datasets/make_dataset.sh
+chmod +x make_dataset.sh
+./make_dataset.sh
 
 cd ..
 pip3 install torch torchvision torchaudio
