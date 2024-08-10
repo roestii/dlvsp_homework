7a4e9bd74431fad7059c2986535eaea1850fe292
Louis Betsch <louisbetsch.b@gmail.com>
2024-08-10 18:20:22 +0200

    added initial setup file for downloading all required files

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo loading latest imagenet checkpoint
+curl -L https://dl.fbaipublicfiles.com/ijepa/IN1K-vit.h.14-300e.pth.tar > checkpoints/IN1K-vit.h.14-300e.pth.tar
+
+cd datasets
+echo loading dataset
+chmod +x make_dataset.sh
+./make_dataset.sh
+
+cd ..
+pip3 install torch torchvision torchaudio
