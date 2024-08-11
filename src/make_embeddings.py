79a3675394aebc97530ad749ac485611bfe035b5
Louis Betsch <louisbetsch.b@gmail.com>
2024-08-11 18:16:09 +0200

    trying to optimize embeddings creation

--- src/make_embeddings.py
+++ src/make_embeddings.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np 
+import torch
 
 from src.datasets.food101 import make_food101
 from src.models import vision_transformer
@@ -15,33 +16,33 @@ def main(args):
     out_folder = args["out_folder"]
     batch_size = args["batch_size"]
 
-    encoder = vision_transformer.__dict__[model_name](
-        img_size=[crop_size],
-        patch_size=patch_size)
-    encoder = load_encoder(encoder, checkpoint_path)
-    transforms = make_transforms(crop_size=crop_size)
+    with torch.no_grad():
+        encoder = vision_transformer.__dict__[model_name](
+            img_size=[crop_size],
+            patch_size=patch_size)
+        encoder = load_encoder(encoder, checkpoint_path)
+        transforms = make_transforms(crop_size=crop_size)
 
-    for mode in ["val/", "test/"]:
-        os.mkdir(os.path.join(out_folder, mode))
-        dataset, dataloader = make_food101(
-            transforms, 
-            batch_size, 
-            mode="val/",
-            root_path="",
-            image_folder=image_folder
-        )
+        for mode in ["val/", "test/"]:
+            data_path = os.path.join(out_folder, mode)
+            if not os.path.exists(data_path):
+                os.mkdir(os.path.join(out_folder, mode))
 
-        for cl in dataset.classes:
-            path = os.path.join(out_folder, mode, cl)
-            os.mkdir(path)
+            dataset, dataloader = make_food101(
+                transforms, 
+                batch_size, 
+                mode="val/",
+                root_path="",
+                image_folder=image_folder
+            )
 
-        it = iter(dataloader)
-        for i, (xs, ys) in enumerate(it):
-            print(f"iteration {i}")
-            embeddings = encoder(xs)
-            for k in range(len(embeddings)):
-                cl = dataset.classes[ys[k]]
-                path = os.path.join(out_folder, mode, cl, f"{i}_{k}.npy")
-                with open(path, "wb") as file:
-                    embedding = embeddings[i].detach().numpy()
-                    np.save(file, embedding)
+            it = iter(dataloader)
+            for i, (x, y) in enumerate(it):
+                print(i)
+                embedding = encoder(x)
+                print("encoded")
+                emb_path = os.path.join(data_path, f"embedding_{i}.pth")
+                y_path = os.path.join(data_path, f"class_{i}.pth")
+                torch.save(embedding, emb_path)
+                torch.save(y, y_path)
+                del embedding
